b78ea9abcd832a6f2a8752389165d38a64fb3eb4
adam-l <frontend.zorro@gmail.com>
2015-07-14 20:43:21 +0100

    Using ES6 arrow functions now

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 import gulp        from 'gulp';
-import remove      from 'gulp-rimraf';
 import eventStream from 'event-stream';
 
-import nameLint    from 'name-lint';
+import remove      from 'gulp-rimraf';
+import concat      from 'gulp-concat';
+
+import nameLint    from require('name-lint');
 
 import htmlHint    from 'gulp-htmlhint';
 import htmlW3C     from 'gulp-w3cjs';
@@ -11,7 +13,6 @@ import htmlWCAG    from 'gulp-accessibility';
 import sass        from 'gulp-sass';
 import scsslint    from 'gulp-scss-lint';
 import minifyCSS   from 'gulp-minify-css';
-import concat      from 'gulp-concat';
 
 var paths = {
     files: {
@@ -27,7 +28,7 @@ var paths = {
 
 
 /* File naming */
-gulp.task('name-lint', function (callback) {
+gulp.task('name-lint', callback => {
     var regularExpression = /^_?[a-z0-9\-]+$/;
 
     nameLint('src/', {
@@ -38,7 +39,7 @@ gulp.task('name-lint', function (callback) {
         'exclude': [
             'node_modules'
         ],
-    }, function (error, matches) {
+    }, (error, matches) => {
         if (error) {
             throw error
         }
@@ -50,18 +51,18 @@ gulp.task('name-lint', function (callback) {
 
 
 /* HTML */
-gulp.task('html-hint', function () {
+gulp.task('html-hint', () => {
     return gulp.src(paths.files.html)
             .pipe(htmlHint())
             .pipe(htmlHint.reporter());
 });
 
-gulp.task('html-w3c', function () {
+gulp.task('html-w3c', () => {
     return gulp.src(paths.files.html)
             .pipe(htmlW3C());
 });
 
-gulp.task('html-wcag', function () {
+gulp.task('html-wcag', () => {
     return gulp.src(paths.files.html)
             .pipe(htmlWCAG({}));
 });
@@ -70,12 +71,12 @@ gulp.task('test:html', ['html-hint', 'html-w3c', 'html-wcag']);
 
 
 /* Stylesheets */
-gulp.task('css-clean', function () {
+gulp.task('css-clean', () => {
     return gulp.src(paths.folders.css + 'main.css')
             .pipe(remove());
 });
 
-gulp.task('css-concat', ['css-clean'], function () {
+gulp.task('css-concat', ['css-clean'], () => {
     var compiled = gulp.src(paths.files.scss).pipe(sass()),
         css      = gulp.src(paths.files.css);
 
@@ -84,24 +85,24 @@ gulp.task('css-concat', ['css-clean'], function () {
             .pipe(gulp.dest(paths.folders.css));
 });
 
-gulp.task('css-minify', ['css-concat'], function () {
+gulp.task('css-minify', ['css-concat'], () => {
     return gulp.src(paths.folders.css + 'main.css')
             .pipe(minifyCSS())
             .pipe(gulp.dest(paths.folders.css));
 });
 
-gulp.task('scss-lint', function () {
+gulp.task('scss-lint', () => {
     return gulp.src(paths.files.scss)
             .pipe(scsslint());
 });
 
 
 /* Watch */
-gulp.task('watch:html', function () {
+gulp.task('watch:html', () => {
     gulp.watch([paths.files.html], ['html-check']);
 });
 
-gulp.task('watch:css', function () {
+gulp.task('watch:css', () => {
     gulp.watch([paths.files.scss], ['scss-lint']);
     gulp.watch([paths.files.scss, paths.files.css], ['css-concat']);
 });
